461fd06e1eae7758a25cb07012fffb41659e7c6e
Viktor Luthman <Viktorll95@hotmail.com>
2024-11-10 10:57:15 +0100

    Updated music funtions

--- script1.js
+++ script1.js
@@ -7,27 +7,40 @@ const mysticForestAudio = new Audio("mystic-forest-ambient.mp3");
 const cricketsNightAudio = new Audio("crickets-at-night.mp3");
 
 let music;
+let musicPlaying = false;
 
+//FIXME: New issue: If music is playing and you press the active musics "Play"-button, the music should pause and the play button should return to default (The "play" img should appear)
 document
   .querySelector("#btn-mystic-forest")
   .addEventListener("click", function () {
+    if (musicPlaying) {
+      resetAllToPlay();
+      music.pause();
+    }
     music = mysticForestAudio;
     music.play();
+    musicPlaying = true;
   });
 document
   .querySelector("#btn-crickets-night")
   .addEventListener("click", function () {
+    if (musicPlaying) {
+      resetAllToPlay();
+      music.pause();
+    }
     music = cricketsNightAudio;
     music.play();
+    musicPlaying = true;
   });
 
+// Just an event listener attached to the title of the page
 document.querySelector(".c-title").addEventListener("click", function () {
   music.pause();
 });
 
 // Function to reset all images to "play-circle-fill.svg"
 function resetAllToPlay() {
-  const images = document.querySelectorAll(".c-row button img");
+  const images = document.querySelectorAll(".m-row button img");
   images.forEach((img) => {
     img.src = "play-circle-fill.svg";
   });
